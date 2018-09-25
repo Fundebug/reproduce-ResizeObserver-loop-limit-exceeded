7f5c80df3ff75ee067bf9afaaee911d527b912fc
stefanzan <stefanzan@163.com>
2018-09-25 10:07:40 +0800

    test case for ResizeObserver loop limit exceed error

---
+++ src/App.vue
@@ -0,0 +1,125 @@
+<template>
+  <div id="app">
+    <template>
+      <el-select v-model="value" placeholder="请选择" @change="selectValue">
+        <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
+        </el-option>
+      </el-select>
+    </template>
+
+    <el-table :data="tableData" border style="width: 100%;">
+      <template v-for="(item, index) in tableHeaders">
+        <el-table-column :key="index" :prop="item.key" :label="item.label" header-align="center">
+        </el-table-column>
+      </template>
+    </el-table>
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      options: [
+        {
+          value: "1",
+          label: "文保行业"
+        },
+        {
+          value: "2",
+          label: "办公场所"
+        },
+        {
+          value: "3",
+          label: "街道"
+        }
+      ],
+      value: "",
+      tableData: [
+        { 1: "a", 2: "男", 3: 134567756554 },
+        { 1: "a", 2: "男", 3: 134567756554 },
+        { 1: "a", 2: "男", 3: 134567756554 },
+        { 1: "a", 2: "男", 3: 134567756554 },
+        { 1: "a", 2: "男", 3: 134567756554 }
+      ],
+      tableHeaders: [
+        {
+          key: "1",
+          label: "名字"
+        },
+        {
+          key: "2",
+          label: "性别"
+        },
+        {
+          key: "3",
+          label: "手机号"
+        },
+        {
+          key: "4",
+          label: "地址"
+        }
+      ]
+    };
+  },
+  methods: {
+    selectValue() {
+      if (this.value === "1") {
+        this.tableData = [
+          { 1: "a", 2: "男", 3: 134567756554 },
+          { 1: "a", 2: "男", 3: 134567756554 },
+          { 1: "a", 2: "男", 3: 134567756554 },
+          { 1: "a", 2: "男", 3: 134567756554 },
+          { 1: "a", 2: "男", 3: 134567756554 }
+        ];
+      } else {
+        this.tableData = [
+          {
+            1: "a",
+            2: "男",
+            3: 134567756554,
+            4: "ResizeObserver(entries => { for (let entry of entries) { if (entry.target === target) ."
+          },
+          {
+            1: "b",
+            2: "男",
+            3: 134567756554,
+            4: "ResizeObserver(entries => { for (let entry of entries) { if (entry.target === target) ."
+          },
+          {
+            1: "c",
+            2: "男",
+            3: 134567756554,
+            4: "ResizeObserver(entries => { for (let entry of entries) { if (entry.target === target) ."
+          },
+          {
+            1: "d",
+            2: "男",
+            3: 134567756554,
+            4: "ResizeObserver(entries => { for (let entry of entries) { if (entry.target === target) ."
+          },
+          {
+            1: "e",
+            2: "男",
+            3: 134567756554,
+            4: "ResizeObserver(entries => { for (let entry of entries) { if (entry.target === target) ."
+          }
+        ];
+      }
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,54 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import Element from 'element-ui'
+
+// //样式文件，需单独引入
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.config.productionTip = false
+Vue.use(Element)
+
+window.onerror = function(message, source, lineno, colno, error){
+  console.log(message, source, lineno, colno, error)
+}
+
+// var fundebug=require("fundebug-javascript")
+// fundebug.apikey="39dc6f70c775b4c4e08d304b8918ecec16b354635f1f5567864ce324e50d6816"
+// fundebug.silentBehavior = true
+// fundebug.silentHttp = true
+// fundebug.silentResource = true
+// fundebug.silentConsole = true
+
+// function formatComponentName(vm)
+// {
+//   if (vm.$root === vm) return 'root';
+
+//   var name = vm._isVue ? (vm.$options && vm.$options.name) || (vm.$options && vm.$options._componentTag) : vm.name;
+//   return (name ? 'component <' + name + '>' : 'anonymous component') + (vm._isVue && vm.$options && vm.$options.__file ? ' at ' + (vm.$options && vm.$options.__file) : '');
+
+// }
+
+// Vue.config.errorHandler = function(err, vm, info)
+// {
+//   var componentName = formatComponentName(vm);
+//   var propsData = vm.$options && vm.$options.propsData;
+
+//   fundebug.notifyError(err,
+//   {
+//       metaData:
+//       {
+//           componentName: componentName,
+//           propsData: propsData,
+//           info: info
+//       }
+//    });
+// };
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
